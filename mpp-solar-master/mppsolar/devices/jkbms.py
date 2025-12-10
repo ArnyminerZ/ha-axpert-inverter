983b5b418ede4cf23aca581ded5d0dc8d9a89525
Arnau Mora Gras <arnyminerz@proton.me>
2025-12-10 15:15:54 +0100

    Remove dir

--- mpp-solar-master/mppsolar/devices/jkbms.py
+++
@@ -1,7 +0,0 @@
-from .device import AbstractDevice
-
-
-class jkbms(AbstractDevice):
-    def __init__(self, *args, **kwargs) -> None:
-        self._classname = "jkbms"
-        super().__init__(*args, **kwargs)
--- mpp-solar-master/mppsolar/devices/mppsolar.py
+++
@@ -1,7 +0,0 @@
-from .device import AbstractDevice
-
-
-class mppsolar(AbstractDevice):
-    def __init__(self, *args, **kwargs) -> None:
-        self._classname = "mppsolar"
-        super().__init__(*args, **kwargs)
